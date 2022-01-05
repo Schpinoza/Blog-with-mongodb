a1bba47e24f02bbe0ded80e8b6966c9fc397d6b5
Schpinoza <talgab121@gmail.com>
2022-01-05 15:10:39 +0200

    second commit

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+const posts = [];
 
 app.set('view engine', 'ejs');
 
@@ -20,10 +21,11 @@ app.use(express.static("public"));
 app.use(express.json());
 
 app.get("/", function (req, res) {
-  // res.render("home")
   res.render("home", {
-    homeP: homeStartingContent
+    homeP: homeStartingContent,
+    postArray: posts
   })
+  
 
 });
 
@@ -45,14 +47,15 @@ app.get("/compose", function (req, res) {
 
 });
 
-app.post("/", function (request, response) {
-  let title = request.body.addTitle
-  let body = request.body.addBody
-  let post = {
+app.post("/", function (req, res) {
+  const title = req.body.addTitle;
+  const body = req.body.addBody;
+  const post = {
     title,
     body
-  }
-  console.log(post);
+  };
+  posts.push(post);
+  res.redirect("/")
 })
 
 
